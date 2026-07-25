490bf19266b39a1db5bcf7d91fdc849a0099ca35
onepc903-lab <onepc903@gmail.com>
2026-07-25 01:55:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-
+import barcode
+from barcode.writer import ImageWriter
+from tkinter import messagebox
 root = tk.Tk()
 root.title("EAN13 Barcode Generator")
 root.geometry("700x500")
@@ -20,7 +22,29 @@ ttk.Label(root, text="EAN-13 Number").pack()
 barcode_entry = ttk.Entry(root, width=50)
 barcode_entry.pack(pady=5)
 
-generate_btn = ttk.Button(root, text="Generate Barcode")
+def generate_barcode():
+    number = barcode_entry.get().strip()
+
+    if len(number) != 12 or not number.isdigit():
+        messagebox.showerror(
+            "Error",
+            "Please enter exactly 12 digits.\nThe 13th check digit will be generated automatically."
+        )
+        return
+
+    ean = barcode.get("ean13", number, writer=ImageWriter())
+    filename = ean.save("barcode")
+
+    messagebox.showinfo(
+        "Success",
+        f"Barcode saved successfully:\n{filename}"
+    )
+
+generate_btn = ttk.Button(
+    root,
+    text="Generate Barcode",
+    command=generate_barcode
+)
 generate_btn.pack(pady=20)
 
 root.mainloop()
